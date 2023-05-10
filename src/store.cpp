02de241e2a70a412e6977452ae5e3bf88c8648ed
Alexyy <101402577+ItsNotAlexy@users.noreply.github.com>
2023-05-10 20:09:09 +0700

    Fix bugs with the payment system!

--- src/store.cpp
+++ src/store.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <string>
 #include <math.h>
+#include <windows.h>
+#include <algorithm>
 
 using namespace std;
 
-// Function Prototypes
 int GetUserBalance();
-void SetUserBalance(int balance);
-string Withdraw(int amount);
-string Deposit(int amount);
-string Pay(int total);
-string CheckCoupon(string code, const string* coupon);
-string MakeReceipt(const string* products, const int* prices, const int* quantity, int n, bool discount);
+string ExtractFromReciept(const string &ReceiptVar);
+string SetUserBalance(int NewBalance);
+string Withdraw(int WithdrawAmount);
+string Deposit(int DepositAmount);
+string CheckCoupon(string code, const string *coupon);
+string MakeReceipt(const string *products, const int *prices, const int *quantity, int n, bool discount);
 
-int main(){
-    // Variables
+int main()
+{
     string products[5] = {"Milk", "Bread", "Eggs", "Cheese", "Butter"};
     string coupon[5] = {"F05GT", "G05GT", "H05GT", "I05GT", "J05GT"};
     int prices[5] = {5, 5, 3, 6, 3};
@@ -22,61 +23,99 @@ int main(){
     int total = 0;
     int choice = 0;
 
-    // Welcome GUI
     cout << "=============================" << endl;
-    cout << "Store System v3.0" << endl;
+    cout << "Store System v2.0" << endl;
     cout << "=============================" << endl;
     cout << "Press 9 for ATM" << endl;
-    cout << "=============================" << endl;
-    cout << "Here is a list of our products: " << endl;
-    for(int i = 0; i < 5; i++){
-        cout << i + 1 << ". " << products[i] << " - $" << prices[i] << endl;
-    }
-    cout << endl;
-    cout << "Please enter the number of the product you would like to buy" << endl;
-    cout << "=============================" << endl;
-    cout << "Enter 0 to checkout" << endl;
+    cout << "Press 8 for product list" << endl;
     cout << "=============================" << endl;
 
-    while(true){
+    while (true)
+    {
         cout << "Enter Option: ";
         cin >> choice;
 
-        if(choice == 0){
-            // Checkout
-            cout << "=============================" << endl;
-            cout << "Here is your receipt: " << endl;
-            cout << MakeReceipt(products, prices, quantity, 5, false) << endl;
-            cout << "=============================" << endl;
-            cout << "Please enter your coupon code: ";
-            string code;
-            cin >> code;
-            cout << "=============================" << endl;
-            string IsValid = CheckCoupon(code, coupon);
-            if(IsValid == "Valid"){
-                cout << "Here is your receipt with discount: " << endl;
-                cout << MakeReceipt(products, prices, quantity, 5, true) << endl;
-            } else {
-                cout << "Your coupon is " << IsValid << endl;
-                cout << "Here is your receipt without discount: " << endl;
-                cout << MakeReceipt(products, prices, quantity, 5, false) << endl;
-            }
-            // Automatic payment
-            string PaymentStatus = Pay(total);
-            
-            if(PaymentStatus == "Success"){
-                cout << "Your payment was successful" << endl;
-            } else {
-                cout << "Your payment was " << PaymentStatus << endl;
-                break;
-            }
+        if (choice == 8)
+        {
+            system("cls");
 
-            cout << "Thank you for shopping with us!" << endl;
             cout << "=============================" << endl;
-            break;
+            cout << "Here is the product list: " << endl;
+            cout << "=============================" << endl;
+            for (int i = 0; i < 5; i++)
+            {
+                cout << i + 1 << ". " << products[i] << " - $" << prices[i] << endl;
+            }
+            cout << "=============================" << endl;
+            int ProductChoice = -1;
+
+            while (ProductChoice != 0)
+            {
+                cout << "Please select a product: ";
+                cin >> ProductChoice;
+
+                if (ProductChoice == 0)
+                {
+                    cout << "=============================" << endl;
+                    cout << "Here is your receipt: " << endl;
+                    cout << MakeReceipt(products, prices, quantity, 5, false) << endl;
+                    cout << "=============================" << endl;
+                    cout << "Please enter your coupon code: ";
+                    string code;
+                    cin >> code;
+                    cout << "=============================" << endl;
+                    string IsValid = CheckCoupon(code, coupon);
+                    if (IsValid == "Valid")
+                    {
+                        cout << "Here is your receipt with discount: " << endl;
+                        cout << MakeReceipt(products, prices, quantity, 5, true) << endl;
+                    }
+                    else
+                    {
+                        cout << "Your coupon is " << IsValid << endl;
+                        cout << "Here is your receipt without discount: " << endl;
+                        cout << MakeReceipt(products, prices, quantity, 5, false) << endl;
+                    }
+
+                    string ReceiptVar = MakeReceipt(products, prices, quantity, 5, false);
+                    string Total = ExtractFromReciept(ReceiptVar);
+                    int StringToInt = stoi(Total);
+
+                    if (GetUserBalance() < StringToInt)
+                    {
+                        cout << "You have insufficient funds!" << endl;
+                        cout << "All items have been removed from your cart" << endl;
+                        return 0;
+                    }
+                    else
+                    {
+                        SetUserBalance(GetUserBalance() - StringToInt);
+                        cout << "Processing payment..." << endl;
+                        Sleep(2000);
+                        cout << "Payment successful! Your new balance is $" << GetUserBalance() << endl;
+                        cout << "=============================" << endl;
+                        return 0;
+                    }
+
+                    return 0;
+                }
+                else if (ProductChoice > 5)
+                {
+                    cout << "Invalid product, please try again" << endl;
+                    cout << "=============================" << endl;
+                }
+                else
+                {
+                    cout << "How many " << products[ProductChoice - 1] << " would you like to buy: ";
+                    cin >> quantity[ProductChoice - 1];
+                    cout << "=============================" << endl;
+                    cout << "You have added " << quantity[ProductChoice - 1] << " " << products[ProductChoice - 1] << " to your cart" << endl;
+                    cout << "=============================" << endl;
+                }
+            }
         }
-        if(choice == 9){
-            // ATM
+        if (choice == 9)
+        {
             system("cls");
 
             cout << "=============================" << endl;
@@ -86,12 +125,14 @@ int main(){
             int pin;
             cin >> pin;
             cout << "=============================" << endl;
-            while(pin != 1234){
+            while (pin != 1234)
+            {
                 cout << "Invalid pin, please try again: ";
                 cin >> pin;
                 cout << "=============================" << endl;
             }
-            while(pin == 1234){
+            while (pin == 1234)
+            {
                 system("cls");
 
                 int balance = GetUserBalance();
@@ -110,7 +151,8 @@ int main(){
                 cin >> AtmOption;
 
                 cout << "=============================" << endl;
-                if(AtmOption == "withdraw"){
+                if (AtmOption == "withdraw")
+                {
                     system("cls");
 
                     int WithdrawAmount;
@@ -118,16 +160,20 @@ int main(){
                     cin >> WithdrawAmount;
 
                     string WithdrawStatus = Withdraw(WithdrawAmount);
-                    if(WithdrawStatus == "Success"){
+                    if (WithdrawStatus == "Success")
+                    {
                         balance -= WithdrawAmount;
                         cout << "Your new balance is $" << balance << endl;
                         cout << "=============================" << endl;
-                    } else {
+                    }
+                    else
+                    {
                         cout << "Your withdrawal was " << WithdrawStatus << endl;
                         cout << "=============================" << endl;
                     }
                 }
-                else if(AtmOption == "deposit"){
+                else if (AtmOption == "deposit")
+                {
                     system("cls");
 
                     int DepositAmount;
@@ -135,39 +181,56 @@ int main(){
                     cin >> DepositAmount;
 
                     string DepositStatus = Deposit(DepositAmount);
-                    if(DepositStatus == "Success"){
+                    if (DepositStatus == "Success")
+                    {
                         balance += DepositAmount;
                         cout << "Your new balance is $" << balance << endl;
                         cout << "=============================" << endl;
-                    } else {
+                    }
+                    else
+                    {
                         cout << "Your deposit was " << DepositStatus << endl;
                         cout << "=============================" << endl;
                     }
                 }
-                else if(AtmOption == "check-balance"){
+                else if (AtmOption == "check-balance")
+                {
                     system("cls");
 
                     cout << "Your balance is $" << balance << endl;
                     cout << "=============================" << endl;
                 }
-                else if(AtmOption == "pay"){
+                else if (AtmOption == "pay")
+                {
                     system("cls");
 
                     int PayAmount;
                     cout << "How much would you like to pay: ";
                     cin >> PayAmount;
 
-                    string PayStatus = Pay(PayAmount);
-                    if(PayStatus == "Success"){
-                        balance -= PayAmount;
-                        cout << "Your new balance is $" << balance << endl;
+                    string IntToString = to_string(PayAmount);
+
+                    if (PayAmount > GetUserBalance())
+                    {
+                        cout << "You do not have enough money to pay!" << endl;
                         cout << "=============================" << endl;
-                    } else {
-                        cout << "Your payment was " << PayStatus << endl;
+                    }
+                    else if (!all_of(IntToString.begin(), IntToString.end(), ::isdigit))
+                    {
+                        cout << "Invalid amount" << endl;
+                        cout << "=============================" << endl;
+                    }
+                    else
+                    {
+                        SetUserBalance(GetUserBalance() - GetUserBalance());
+                        cout << "Processing payment..." << endl;
+                        Sleep(2000);
+                        cout << "Payment successful! Your new balance is $" << GetUserBalance() << endl;
                         cout << "=============================" << endl;
                     }
                 }
-                else if(AtmOption == "exit"){
+                else if (AtmOption == "exit")
+                {
                     system("cls");
 
                     cout << "Thank you for using the ATM" << endl;
@@ -175,39 +238,44 @@ int main(){
                     break;
                 }
             }
-        } else {
-            cout << "How many " << products[choice - 1] << " would you like to buy: ";
-            cin >> quantity[choice - 1];
-            cout << "=============================" << endl;
-            cout << "You have added " << quantity[choice - 1] << " " << products[choice - 1] << " to your cart" << endl;
-            cout << "=============================" << endl;
         }
-}
+        else
+        {
+            cout << "Invalid option, please try again" << endl;
+        }
+    }
     return 0;
 }
 
-// Checking if a coupon is valid
-string CheckCoupon(string code, const string* coupon){
+string CheckCoupon(string code, const string *coupon)
+{
     int length = 5;
-    for(int i = 0; i < length; i++){
-        if(code == coupon[i]){
+    for (int i = 0; i < length; i++)
+    {
+        if (code == coupon[i])
+        {
             return "Valid";
         }
     }
     return "Invalid";
 }
 
-// Making a receipt
-string MakeReceipt(const string* products, const int* prices, const int* quantity, int n, bool discount) {
+string MakeReceipt(const string *products, const int *prices, const int *quantity, int n, bool discount)
+{
     string receipt = "";
     int total = 0;
-    for(int i = 0; i < n; i++) {
-        if(quantity[i] > 0) {
+    for (int i = 0; i < n; i++)
+    {
+        if (quantity[i] > 0)
+        {
             receipt += products[i] + " - " + to_string(quantity[i]) + " - $" + to_string(prices[i] * quantity[i]) + "\n";
-            if(discount){
+            if (discount)
+            {
                 receipt += "Discount - 10%\n";
                 total += prices[i] * quantity[i] * 0.9;
-            }else{
+            }
+            else
+            {
                 total += prices[i] * quantity[i];
             }
         }
@@ -216,38 +284,67 @@ string MakeReceipt(const string* products, const int* prices, const int* quantit
     return receipt;
 }
 
-// Balance Variable
+string ExtractFromReciept(const string &ReceiptVar)
+{
+    string Total = "";
+    int length = ReceiptVar.length();
+    for (int i = 0; i < length; i++)
+    {
+        if (ReceiptVar[i] == 'T' && ReceiptVar[i + 1] == 'o' && ReceiptVar[i + 2] == 't' && ReceiptVar[i + 3] == 'a' && ReceiptVar[i + 4] == 'l')
+        {
+            for (int j = i + 8; j < length; j++)
+            {
+                if (ReceiptVar[j] == '\n')
+                {
+                    break;
+                }
+                else
+                {
+                    Total += ReceiptVar[j];
+                }
+            }
+        }
+    }
+    Total.erase(0, 1);
+    return Total;
+}
+
 int Bankbalance = 1000;
 
-int GetUserBalance(){
+int GetUserBalance()
+{
     return Bankbalance;
 }
 
-void SetUserBalance(int balance){
-    Bankbalance = balance;
-}
-
-// ATM Functions
-
-string Withdraw(int amount){
-    if(amount > GetUserBalance()){
-        return "Insufficient Funds";
-    } else {
-        SetUserBalance(GetUserBalance() - amount);
+string SetUserBalance(int NewBalance)
+{
+    if (NewBalance < 0)
+    {
+        return "Unsuccess";
+    }
+    else
+    {
+        Bankbalance = NewBalance;
         return "Success";
     }
 }
 
-string Deposit(int amount){
-    SetUserBalance(GetUserBalance() + amount);
+string Withdraw(int WithdrawAmount)
+{
+    if (WithdrawAmount > GetUserBalance())
+    {
+        return "Unsuccess";
+    }
+    else
+    {
+        SetUserBalance(GetUserBalance() - WithdrawAmount);
+        return "Success";
+    }
+}
+
+string Deposit(int DepositAmount)
+{
+    SetUserBalance(GetUserBalance() + DepositAmount);
     return "Success";
 }
 
-string Pay(int total){
-    if(total > GetUserBalance()){
-        return "Insufficient Funds";
-    } else {
-        SetUserBalance(GetUserBalance() - total);
-        return "Success";
-    }
-}
